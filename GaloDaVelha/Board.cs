5aa5222e9717ca9774c3b1abb505c819e57fc6aa
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-04-03 23:54:38 +0100

    Added PiecePlacer method, it receives the piece and its coordinates

--- GaloDaVelha/Board.cs
+++ GaloDaVelha/Board.cs
@@ -66,6 +66,41 @@ namespace GaloDaVelha
                 Console.WriteLine(strBoard);
             }
         }
+        /// <summary>
+        /// This method accepts a piece chosen by a player and its coordinates,
+        /// then puts the piece at those coordinates if they are free.
+        /// </summary>
+        /// <param name="_piece">This represent the chosen piece</param>
+        /// <param name="coord">This are the (x,y) coordinates of the piece</param>
+        public void PiecePlacer(Piece _piece, int[] coord)
+        {
+            if (coord.Length != 2) //Guarantees that the coordinates are correct
+            {
+                Console.WriteLine("Need a valid (x,y) position to continue!");
+                return;
+            }
+            
+            {   
+                //Runs through all of piecesLeft array
+                for (int i = 0; i < piecesLeft.Length; i++)
+                
+                {   
+                    //Checks if the piece chosen is correspondent to a piece in piecesLeft 
+                    if (piecesLeft[i] == _piece) 
+                    {
+                        //Puts the piece at the given coordinates
+                        board[coord[0], coord[1]] = _piece;
+
+                        //The piece is "marked" as used
+                        piecesLeft[i] = null;
+
+                        break; //Exit the loop, which means that the move ends
+                    }
+                }
+            }
+        }
+     
     }
 
 }
+
